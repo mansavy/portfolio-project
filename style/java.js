87826cdb97c101bdc0245b1b53bce2abc4dfb579
mansavy <100310641+mansavy@users.noreply.github.com>
2022-04-15 14:13:53 -0300

    Styled work page buttons

--- style/java.js
+++ style/java.js
@@ -1,31 +1,31 @@
 function hide() {
-  let button = document.querySelector(".more-button");
+  let button = document.querySelector("#moreButton");
   button.style.display = "none";
 }
 
 function show() {
-  let button = document.querySelector(".more-button");
+  let button = document.querySelector("#moreButton");
   button.style.display = "block";
 }
 
 function hideWeb() {
-  let button = document.querySelector(".web-more-button");
+  let button = document.querySelector("#webMoreButton");
   button.style.display = "none";
 }
 
 function showWeb() {
-  let button = document.querySelector(".web-more-button");
+  let button = document.querySelector("#webMoreButton");
   button.style.display = "block";
 }
 
-let button = document.querySelector(".more-button");
+let button = document.querySelector("#moreButton");
 button.addEventListener("click", hide);
 
-let less = document.querySelector(".less-button");
+let less = document.querySelector("#lessButton");
 less.addEventListener("click", show);
 
-let buttonMore = document.querySelector(".web-more-button");
+let buttonMore = document.querySelector("#webMoreButton");
 buttonMore.addEventListener("click", hideWeb);
 
-let lessLess = document.querySelector(".web-less-button");
+let lessLess = document.querySelector("#webLessButton");
 lessLess.addEventListener("click", showWeb);
