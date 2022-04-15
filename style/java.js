fc41687f62c4233e73cde234b60be59287ac50b8
mansavy <100310641+mansavy@users.noreply.github.com>
2022-04-15 00:43:11 -0300

    Added content to web work section

--- style/java.js
+++ style/java.js
@@ -8,8 +8,24 @@ function show() {
   button.style.display = "block";
 }
 
+function hideWeb() {
+  let button = document.querySelector(".web-more-button");
+  button.style.display = "none";
+}
+
+function showWeb() {
+  let button = document.querySelector(".web-more-button");
+  button.style.display = "block";
+}
+
 let button = document.querySelector(".more-button");
 button.addEventListener("click", hide);
 
 let less = document.querySelector(".less-button");
 less.addEventListener("click", show);
+
+let buttonMore = document.querySelector(".web-more-button");
+buttonMore.addEventListener("click", hideWeb);
+
+let lessLess = document.querySelector(".web-less-button");
+lessLess.addEventListener("click", showWeb);
