e396e63c7395f8f405989a3ef52e2b763f8d5c7d
mansavy <100310641+mansavy@users.noreply.github.com>
2022-04-16 21:20:54 -0300

    Added javascript to nav bar

--- style/javaindex.js
+++ style/javaindex.js
@@ -1,8 +1,15 @@
 let navbar = document.querySelector(".navbar");
+
 window.onscroll = () => {
   if (window.scrollY > 650 && window.scrollY < 1350) {
     navbar.classList.add("nav-active");
+    navbar.classList.remove("nav-second");
+  } else if (window.scrollY > 1350) {
+    navbar.classList.add("nav-second");
+    document.getElementById("brand").style.display = "none";
   } else {
+    navbar.classList.remove("nav-second");
     navbar.classList.remove("nav-active");
+    document.getElementById("brand").style.display = "block";
   }
 };
