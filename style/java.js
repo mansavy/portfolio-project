b39a598709b1dccd1fba13f3897f050c9cce9e0e
mansavy <100310641+mansavy@users.noreply.github.com>
2022-04-17 15:00:16 -0300

    Added javascript to work navbar

--- style/java.js
+++ style/java.js
@@ -29,3 +29,15 @@ buttonMore.addEventListener("click", hideWeb);
 
 let lessLess = document.querySelector("#webLessButton");
 lessLess.addEventListener("click", showWeb);
+
+let navbar = document.querySelector(".navbar");
+
+window.onscroll = () => {
+  if (window.scrollY > 85) {
+    navbar.classList.add("nav-second");
+    document.getElementById("brand").style.display = "none";
+  } else {
+    navbar.classList.remove("nav-second");
+    document.getElementById("brand").style.display = "block";
+  }
+};
--- style/javaindex.js
+++ style/javaindex.js
@@ -1,10 +1,10 @@
 let navbar = document.querySelector(".navbar");
 
 window.onscroll = () => {
-  if (window.scrollY > 650 && window.scrollY < 1350) {
+  if (window.scrollY > 615 && window.scrollY < 1330) {
     navbar.classList.add("nav-active");
     navbar.classList.remove("nav-second");
-  } else if (window.scrollY > 1350) {
+  } else if (window.scrollY > 1328) {
     navbar.classList.add("nav-second");
     document.getElementById("brand").style.display = "none";
   } else {
